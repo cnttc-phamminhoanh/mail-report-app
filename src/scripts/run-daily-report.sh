762997c8645dca5b29ca203c72cf2f0dcde9ce17
mati <oanhpham41121@gmail.com>
2026-06-10 23:36:34 +0700

    Update script

--- src/scripts/run-daily-report.sh
+++ src/scripts/run-daily-report.sh
@@ -6,4 +6,4 @@ mkdir -p excel_reports
 
 mkdir -p logs
 
-/home/it/.nvm/versions/node/v22.18.0/bin/npm run daily-report >> logs/cron.log 2>&1
+/home/it/.nvm/versions/node/v22.18.0/bin/node src/scripts/run-daily-report.js >> logs/cron.log 2>&1
