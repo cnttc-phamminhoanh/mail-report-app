f520595908be75dbe5928c3f473417470ce80281
duyen <duyenvo06092003@gmail.com>
2026-06-29 12:55:27 +0700

    Add MRP Plan does not generate Purchase Plan

---
+++ run-job-mrp_pl_not_ch_pl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Thời gian chạy: 03:00 AM - <mrp_pl_not_ch_pl>
+
+cd /home/it/mail-report-app
+
+mkdir -p excel_reports
+mkdir -p logs
+
+JOB_NAME=$1
+
+/home/it/.nvm/versions/node/v22.18.0/bin/node \
+src/scripts/run-job.js \
+"$JOB_NAME" \
+>> logs/${JOB_NAME}.log 2>&1
