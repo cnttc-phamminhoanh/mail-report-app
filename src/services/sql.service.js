dd145c89529480f58103bed5a3833e97313167e7
Nguyen Truc Vi <vi.nt@germton.com.vn>
2026-07-02 16:06:58 +0700

    mnew

--- src/services/sql.service.js
+++ src/services/sql.service.js
@@ -18,18 +18,3 @@ module.exports = {
 }
 
 
-// Thêm hàm này vào trong file src/services/sql.service.js của bạn
-async function executeDynamicQuery(queryString) {
-    try {
-        // Mượn cấu hình connect database có sẵn trong file của bạn
-        // Giả sử biến kết nối của bạn tên là pool hoặc mssql liên kết với src/config/database
-        const pool = await poolPromise; // Bạn xem file đang dùng biến gì để connect thì gọi biến đó nhé
-        const result = await pool.request().query(queryString);
-        return result.recordset;
-    } catch (error) {
-        throw error;
-    }
-}
-
-// Nhớ xuất hàm này ra ở module.exports của file đó nhé:
-// module.exports = { ..., executeDynamicQuery };
