4937faf1dd7815c852a9d5319baed9744d73881d
mati <oanhpham41121@gmail.com>
2026-09-14 14:58:41 +0700

    Increase request timeout

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,20 @@ module.exports = {
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+
+  connectionTimeout: 120000,
+  requestTimeout: 120000,
+  cancelTimeout: 5000,
+
   options: {
     trustServerCertificate: true,
+    connectTimeout: 120000,
+    encrypt: false,
+  },
+
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
   },
 }
